ea0dee68d6cd3b12373e8ef7767bad4cce8fe6c4
yogeshpaisode <yogeshpaisode@192.168.1.100>
2016-09-14 23:37:39 +0530

    Initi

--- web/WEB-INF/test.sql
+++ web/WEB-INF/test.sql
@@ -1,6 +1,11 @@
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
+Logging URL:- https://104.149.239.216:2083
+
+UserName:-  
+
+PassWord:- SvTS$3s$#D54Dy6
  * and open the template in the editor.
  */
 /**
